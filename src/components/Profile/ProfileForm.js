313b7ac37d0bb43bd62421359e4fbd12a426fc41
강한길 <59948163+gksrlf@users.noreply.github.com>
2022-11-30 17:24:23 +0900

    섹션22 완료

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,50 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css"
+import { useContext, useRef } from "react"
+
+import AuthContext from "../../store/auth-context"
+import { useHistory } from "react-router-dom"
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
+	const history = useHistory()
+	const newPasswordInputRef = useRef()
+	const authCtx = useContext(AuthContext)
+	const submitHandler = (event) => {
+		event.preventDefault()
+		const enteredNewPassword = newPasswordInputRef.current.value
+
+		fetch(
+			"https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDd216I2D5UY1V_WYjauaDewropJzukgD4",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					idToken: authCtx.token,
+					password: enteredNewPassword,
+					returnSecureToken: false,
+				}),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		).then((res) => {
+			history.replace("/")
+		})
+	}
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<div className={classes.control}>
+				<label htmlFor="new-password">New Password</label>
+				<input
+					type="password"
+					id="new-password"
+					minLength="7"
+					ref={newPasswordInputRef}
+				/>
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	)
 }
 
-export default ProfileForm;
+export default ProfileForm
